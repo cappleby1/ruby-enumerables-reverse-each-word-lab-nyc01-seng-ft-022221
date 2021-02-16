fc2c697a1cc2b0214a701df112f935f7373a0ef0
Colten Appleby <colten96@gmail.com>
2021-02-16 04:16:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,11 @@
 def reverse_each_word(string)
   
   array = string.split(" ")
+  new_array = []
   
+  array.each do |word|
+    new_array << word.reverse
+  end
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  return new_array.join(' ')
 end
